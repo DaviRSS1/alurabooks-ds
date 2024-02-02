17e4e436366e82ed67da6d5d48fbb04989350df9
Davi <davireghimsalles@gmail.com>
2024-02-02 10:17:48 -0300

    'adicionando_variaveis_do_AbBotao'

--- src/componentes/AbBotao/index.tsx
+++ src/componentes/AbBotao/index.tsx
@@ -1,23 +1,39 @@
 import React from "react"
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 
-const BotaoEstilizado = styled.button`
-    background: #EB9B00;
+export interface AbBotaoProps {
+    texto?: string;
+    tipo?: 'primario' | 'secundario';
+    onClick?: () => void;
+}
+
+const BotaoEstilizado = styled.button<AbBotaoProps>`
+    background: ${(props: AbBotaoProps) => props.tipo === 'primario' ? '#EB9B00' : '#fff'};
     padding: 16px 32px;
     border: solid 2px #EB9B00;
-    color: #fff;
+    color: ${(props: AbBotaoProps) => props.tipo === 'primario' ? '#fff' : '#EB9B00'};;
     font-size: 20px;
     cursor: pointer;
-    &:hover{
-        background: #B87900;
-        border: solid 2px #B87900;
-    }
+    ${(props: AbBotaoProps) => props.tipo === 'primario'
+        ? css`
+            &:hover{
+                background: #B87900;
+                border: solid 2px #B87900;
+            }
+    `
+        : css`
+            &:hover{
+                background: #FFF;
+                border: solid 2px #B87900;
+                color: #B87900;
+            }
+    `}
 `
 
-export const AbBotao = () => {
-    return(
-    <BotaoEstilizado>
-        Clique Aqui!
-    </BotaoEstilizado>
+export const AbBotao = ({ texto, onClick, tipo = 'primario' }: AbBotaoProps) => {
+    return (
+        <BotaoEstilizado onClick={onClick} tipo={tipo}>
+            {texto}
+        </BotaoEstilizado>
     )
 }
